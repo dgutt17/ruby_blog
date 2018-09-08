02a42556a0c364a64f117f0638be9fc501291de8
Dan Gutt <dgutt10@gmail.com>
2018-09-08 13:16:10 -0500

    migrations and posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -28,14 +28,28 @@ class PostsController < ApplicationController
     end
 
     def edit
-
+        @post = Post.find(params[:id])
+        @category = Category.all
     end
 
     def update
+        @post = Post.find(params[:id])
+        if @post.update_attributes(update_params)
+            redirect_to post_path, :notice => "Your post has been updated"
+        else
+            render "edit"
+        end
+    
+    end
 
+    def update_params
+        params.require(:post).permit(:title, :body, :category_id)
     end
 
     def destroy 
+        @post = Post.find(params[:id])
+        @post.destroy
+        redirect_to posts_path, :notice => "Your post has been deleted"
     
     end
 
