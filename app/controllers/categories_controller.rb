f252ba15a3eea3c7175fe5810ab19739fea571b9
Dan Gutt <dgutt10@gmail.com>
2018-09-08 19:23:39 -0500

    ActiveAdmin added

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -5,5 +5,7 @@ class CategoriesController < ApplicationController
 
   def show
     @category = Category.find(params[:id])
+    @title = @category.name
+    @posts = @category.posts # allows you to get all of the posts with that category - created by associations in the models
   end
 end
