c479dfbabf5866567f817a3ab9fd0540bcecc8a0
Dan Gutt <dgutt10@gmail.com>
2018-09-09 12:22:55 -0500

    about to change a lot of style sheets

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'pages/about'
+  get 'pages/contact'
+  get 'pages/resources'
+  get '/about', :to => 'pages#about'
+  get '/contact', :to => 'pages#contact'
+  get '/resources', :to => 'pages#resources'
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   get 'categories/index'
@@ -11,3 +17,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root :to => 'home#index'
 end
+
+# match keyword has been deprecated - just use get instead
